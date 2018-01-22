f5d29a6142c0acd09648fc2eb242346137376c4c
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-01-22 10:08:55 -0600

    Initial commit for 3d-viewer-tool-python directory.
    Script opengl-debugger-utility.py added to 3d-viewer-tool-python project folder.

---
+++ 3d-viewer-tool-python/opengl-debugger-utility.py
@@ -0,0 +1,156 @@
+#!venv/bin/python
+
+import ctypes
+import OpenGL.GL as gl
+import OpenGL.GLUT as glut
+import numpy as np
+import transforms3d as tf
+import sys
+
+
+_vertex_shader_src = 'vertex_shader.glsl'
+_fragment_shader_src = 'fragment_shader.glsl'
+_shader_program = gl.GLuint(0)
+_vbo = 0
+_vao = 0
+_log_buf_sz = 512
+
+
+def idle():
+    pass
+
+
+def mouse_motion_handler(x, y):
+    pass
+
+
+def draw():
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    init_test_object()
+    gl.glUseProgram(_shader_program)
+    gl.glBindVertexArray(_vao)
+    gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
+    # gl.glDrawElements(gl.GL_TRIANGLES, 1, gl.GL_UNSIGNED_INT, 0)
+    gl.glFlush()
+    glut.glutSwapBuffers()
+
+
+def init_shaders():
+    global _shader_program
+    result = True
+    try:
+        # Load vertex shader source and compile.
+        f = open('shaders/' + _vertex_shader_src)
+        vertex_src = f.read()
+        f.close()
+        # create and compile
+        vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+        gl.glShaderSource(vertex_shader, vertex_src)
+        gl.glCompileShader(vertex_shader)
+        # test for shader compile success
+        success = gl.glGetShaderiv(vertex_shader, gl.GL_COMPILE_STATUS)
+        if not success:
+            info_log = gl.glGetShaderInfoLog(vertex_shader)
+            print(info_log.decode('utf-8'))
+            result = False
+        # Load fragment shader source and compile
+        f = open('shaders/' + _fragment_shader_src)
+        fragment_src = f.read()
+        f.close()
+        # create and compile
+        fragment_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
+        gl.glShaderSource(fragment_shader, fragment_src)
+        gl.glCompileShader(fragment_shader)
+        # test for shader compile success
+        success = gl.glGetShaderiv(fragment_shader, gl.GL_COMPILE_STATUS)
+        if not success:
+            info_log = gl.glGetShaderInfoLog(fragment_shader)
+            print(info_log.decode('utf-8'))
+            result = False
+    except IOError as e:
+        print(e.strerror)
+        return False
+    # create shader program
+    _shader_program = gl.glCreateProgram()
+    # attach shaders to the shader program
+    gl.glAttachShader(_shader_program, vertex_shader)
+    gl.glAttachShader(_shader_program, fragment_shader)
+    # link shader program
+    gl.glLinkProgram(_shader_program)
+    # check success of shader program linking
+    success = gl.glGetProgramiv(_shader_program, gl.GL_LINK_STATUS)
+    if not success:
+        info_log = gl.glGetProgramInfoLog(_shader_program)
+        print(info_log)
+        result = False
+    # free shader objects from GPU memory (no longer needed once they have been
+    # linked to the shader program)
+    gl.glDeleteShader(vertex_shader)
+    gl.glDeleteShader(fragment_shader)
+    return result
+
+
+def init_window():
+    glut.glutInitWindowPosition(500, 250)
+    glut.glutInitWindowSize(1000, 500)
+    glut.glutCreateWindow('3D Viewer Tool - TEST PROGRAM')
+
+
+def init_test_object():
+    global _vao, _vbo
+    gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+    verticies = np.ndarray((1, 9), gl.GLfloat,
+                           np.array([-0.5, -0.5, 0,
+                                     0.5, -0.5, 0,
+                                     0, 0.5, 0]))
+    arr = np.array([-0.5, -0.5, 0,
+                    0.5, -0.5, 0,
+                    0, 0.5, 0], dtype=gl.GLfloat)
+    verts = [-0.5, -0.5, 0,
+             0.5, -0.5, 0,
+             0, 0.5, 0]
+    array_type = (gl.GLfloat * len(verts))
+    _vao = gl.glGenVertexArrays(1)
+    _vbo = gl.glGenBuffers(1)
+    gl.glBindVertexArray(_vao)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, _vbo)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, verticies.nbytes,
+                    arr.data,
+                    gl.GL_STATIC_DRAW)
+    gl.glVertexAttribPointer(
+        0, 3, gl.GL_FLOAT, False,
+        3 * ctypes.sizeof(ctypes.c_float), ctypes.c_void_p(0))
+    gl.glEnableVertexAttribArray(0)
+    # Unbind the VAO so that other calls won't accidentally modify this VAO.
+    # May be unnecessary, because another call will have to use
+    # glBindVertexArray to modify a VAO anyway.
+    gl.glBindVertexArray(0)
+
+
+def main():
+    # OpenGL initialization.
+    glut.glutInit(sys.argv)
+    # Initialize buffer and OpenGL settings.
+    # glut.glutInitDisplayMode(
+    #     glut.GLUT_DOUBLE | glut.GLUT_RGB | glut.GLUT_DEPTH)
+    init_window()
+    # Initialize shaders.
+    if not init_shaders():
+        print('Failed to compile and link shader program.')
+    # Initialize and create window, and set GLUT callback functions
+    gl.glEnable(gl.GL_TEXTURE_2D)
+    gl.glEnable(gl.GL_DEPTH_TEST)
+    # gl.glEnable(gl.GL_LIGHTING)   # TODO: Leave light disabled for now
+    # gl.glShadeModel(gl.GL_SMOOTH) #
+    # set display callback function
+    glut.glutDisplayFunc(draw)
+    # set mouse motion callback funciton
+    glut.glutPassiveMotionFunc(mouse_motion_handler)
+    # set idle callback function
+    glut.glutIdleFunc(idle)
+    # Begin main loop.
+    glut.glutMainLoop()
+
+
+if __name__ == '__main__':
+    main()
