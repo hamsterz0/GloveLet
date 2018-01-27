0fcbf3494590e2e6dce38259de93224a08f1d22d
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-01-26 20:25:10 -0600

    Add RectPrismMesh class for easily creating rectangular prisms.

--- 3d-viewer-tool-python/mesh.py
+++ 3d-viewer-tool-python/mesh.py
@@ -1,7 +1,7 @@
-from shadermanager import CURRENT_PROGRAM as shader
 from ctypes import c_uint, c_float, c_void_p, sizeof
 import OpenGL.GL as gl
 import numpy as np
+import shadermanager as sm
 
 DEFAULT_RENDER_MODE = gl.GL_TRIANGLES
 DEFAULT_BUFFER_USAGE = gl.GL_DYNAMIC_DRAW
@@ -58,3 +58,53 @@ class Mesh:
         sz = self.elements.size
         gl.glBindVertexArray(self._vao)
         gl.glDrawElements(self._render_mode, sz, gl.GL_UNSIGNED_INT, None)
+
+
+class RectPrismMesh(Mesh):
+    _width = _height = _depth = 0.5
+    _face_colors = None
+
+    def __init__(self, width, height, depth,
+                 buffer_usage=DEFAULT_BUFFER_USAGE,
+                 face_colors=DEFAULT_COLOR):
+        _width, _height, _depth = width, height, depth
+        verts = np.array([[-width, -height, -depth],
+                          [-width, -height, depth],
+                          [-width, height, depth],
+                          [-width, height, -depth],
+                          [width, height, depth],
+                          [width, height, -depth],
+                          [width, -height, -depth],
+                          [width, -height, depth]],
+                         dtype=c_float)
+        elems = np.array([[4, 5, 6, 7],   # right (+x)
+                          [0, 1, 2, 3],   # left (-x)
+                          [1, 7, 4, 2],   # front (+z)
+                          [6, 0, 3, 5],   # back (-z)
+                          [2, 4, 5, 3],   # top (+y)
+                          [0, 1, 7, 6]],  # bottom (-y)
+                         dtype=c_uint)
+        if not type(face_colors) == np.ndarray and face_colors.shape == (6, 4):
+            self._face_colors = np.zeros((6, 4), c_float)
+            for i in range(face_colors.shape[0]):
+                self._face_colors[i, :] = DEFAULT_COLOR
+        else:
+            self._face_colors = face_colors
+        super().__init__(verts, elems, gl.GL_QUADS, buffer_usage)
+
+    def render(self):
+        # bind Vertex Array Object
+        gl.glBindVertexArray(self._vao)
+        sz = sizeof(c_uint)
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[0])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 0))
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[1])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 4))
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[2])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 8))
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[3])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 12))
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[4])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 16))
+        sm.CURRENT_PROGRAM.color_vec4(self._face_colors[5])
+        gl.glDrawElements(gl.GL_QUADS, 4, gl.GL_UNSIGNED_INT, c_void_p(sz * 20))
