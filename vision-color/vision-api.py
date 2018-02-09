302bdca636e5597a31a78ab11a41ae071f0fa3cf
thearnavgarg <thearnavgarg@gmail.com>
2018-02-08 22:59:35 -0600

    added the find range function

--- vision-color/vision-api.py
+++ vision-color/vision-api.py
@@ -5,54 +5,107 @@ import argparse
 import pyautogui
 import math
 from timeseries import DataTimeSeries
+import logging
+
+def callback(value):
+	pass
+
 
 class Vision():
 
 	FINGER1 = 'finger1'
 	FINGER2 = 'finger2'
-	FINGERS = [FINGER1, FINGER2]
+	ACTIVE_FINGERS = [FINGER1]
+	TOTAL_FINGERS =[FINGER1, FINGER2]
+	TRACKER_FINGER = FINGER1
 
 	def __init__(self):
 		pyautogui.FAILSAFE = False
 		root = tkinter.Tk()
 		root.withdraw()
+		# member variables
+		self.webcam = cv2.VideoCapture(0)
 		self.screen_width = root.winfo_screenwidth()
 		self.screen_height = root.winfo_screenheight()
-		self.webcam = cv2.VideoCapture(0)
 		self.cameraWidth = self.screen_width
 		self.cameraHeight = self.screen_height
-		self.webcam.set(cv2.CAP_PROP_FRAME_WIDTH, self.cameraWidth)
-		self.webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.cameraHeight)
+		# self.webcam.set(cv2.CAP_PROP_FRAME_WIDTH, self.cameraWidth)
+		# self.webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.cameraHeight)
+		self.logger = logging.getLogger(__name__)
 		self.output = {}
 		self.handContour = {}
-		self.window = {self.FINGER1:[], self.FINGER2:[]}
-		self.stationary = {self.FINGER1:False, self.FINGER2:False}
-		self.foundContour = {self.FINGER1:True, self.FINGER2:True}
-		self.realX = {self.FINGER1:int(self.screen_width/2), self.FINGER2:0}
-		self.realY = {self.FINGER1:int(self.screen_height/2), self.FINGER2:0}
+		self.canvas = {}
+		self.handMoment = {}
+		self.foundContour = {}
+		self.realX = {}
+		self.realY = {}
 		self.stationary = False 
 		self.mouseX = self.screen_width/2
 		self.mouseY = self.screen_height/2
 		self.queue = []
 		self.dx = 0
 		self.dy = 0
-		self.canvas = {}
 		self.clickThresh = 70
 		self.clickCounter = 0
 		self.pinched = False
-		self.timeseries = DataTimeSeries(2, 4, auto_filter=True)
-		self.window = np.zeros(4, np.float32)
-		self.boundaries = {self.FINGER1:([23, 96, 50], [38, 252, 227]),
-					  	   self.FINGER2:([131, 69, 0], [181, 255, 255])}
+		self.window = DataTimeSeries(4, 4, auto_filter=True)
+		# self.data = np.zeros(4, np.float32)
+		self.boundaries = {}
+		self.__init_mem_vars()
+
+	def __init_mem_vars(self):
+		# initializing member variables with initial values.
+		for finger in self.TOTAL_FINGERS:
+			self.handMoment[finger] = (0, 0)
+			self.foundContour[finger] = [True]
+			self.realX[finger] = int(self.screen_width/2)
+			self.realY[finger] = int(self.screen_height/2)
+
+		for finger in self.ACTIVE_FINGERS:
+			self.boundaries[finger] = self.__find_range()
+
+	def __find_range(self):
+		range_filter = 'HSV'
+		cv2.namedWindow("Trackbar", 0)
+		for i in ["MIN", "MAX"]:
+			v = 0 if i == "MIN" else 255
+			for j in range_filter:
+				cv2.createTrackbar("%s_%s" % (j, i), "Trackbar", v, 255, callback)
+		while True:
+			_, image = self.webcam.read()
+			image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+			values = []
+			for i in ["MIN", "MAX"]:
+				for j in range_filter:
+					v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbar")
+					values.append(v)
+			lower = (values[:3])
+			upper = (values[3:])
+			thresh = cv2.inRange(image, tuple(lower), tuple(upper))
+			# layout = np.vstack((thresh, image))
+			cv2.imshow('Thresh', thresh)
+			cv2.imshow('Image', image)
+			if cv2.waitKey(1) & 0xFF is ord('q'):
+				cv2.destroyAllWindows()
+				return tuple([lower, upper]) 
 
 	def __read_webcam(self):
+		'''
+		Reading the webcam, flipping the frame vertically by 180 degrees and 
+		creating canvas for all the ACTIVE_FINGERS. Also converting the frame into HSV colorspace
+		'''
 		_, self.frame = self.webcam.read()
 		self.frame = cv2.flip(self.frame, 1)
-		self.canvas[self.FINGER1] = np.zeros(self.frame.shape, np.uint8)
-		self.canvas[self.FINGER2] = np.zeros(self.frame.shape, np.uint8)
+		for finger in self.ACTIVE_FINGERS:
+			self.canvas[finger] = np.zeros(self.frame.shape, np.uint8)
 		self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
 
-	def __add_color_threshold(self, finger):
+	def __threshold(self, finger):
+		'''
+		adding the color threshold to the frame captured by the webcam.
+		The threshold boundaries are specified in advance.
+		'''
+		# TODO: Give user the option to select the threshold color. Under configuration.
 		(lower, upper) = self.boundaries[finger]
 		lower = np.array(lower, dtype="uint8")
 		upper = np.array(upper, dtype="uint8")
@@ -61,6 +114,9 @@ class Vision():
 		self.output[finger] = cv2.cvtColor(self.output[finger], cv2.COLOR_BGR2GRAY)
 	
 	def __extract_contours(self, finger):
+		'''
+		Extracting the contours and finding the maximum amount of area that is being tracked.
+		'''
 		_, self.contours, _ = cv2.findContours(self.output[finger].copy(), cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 		maxArea, idx = 0, 0
 		if len(self.contours) == 0:
@@ -68,6 +124,7 @@ class Vision():
 			return
 		else:
 			self.foundContour[finger] = True
+
 		for i in range(len(self.contours)):
 			area = cv2.contourArea(self.contours[i])
 			if area > maxArea:
@@ -90,24 +147,27 @@ class Vision():
 		if self.moments["m00"] != 0:
 			self.handX = int(self.moments["m10"] / self.moments["m00"])
 			self.handY = int(self.moments["m01"] / self.moments["m00"])
-			self.handMoment = (self.handX, self.handY)
-			self.window[finger].add([self.handMoment])
+			self.handMoment[finger] = (self.handX, self.handY)
 
-	def __find_cursor_location(self, finger):
-		if not self.queue:
-			self.queue.append([self.realX[finger], self.realY[finger]])
-			self.mouseX, self.mouseY = self.realX[finger], self.realY[finger]
-			return
-		if len(self.queue) == 2:
-			del self.queue[0]
-		self.queue.append([self.realX[finger], self.realY[finger]])
-		self.dx = (self.queue[1][0] - self.queue[0][0])**2
-		self.dy = (self.queue[1][1] - self.queue[0][1])**2
+	def __update_cursor(self, coords, finger):
+		self.data = np.zeros(4)
+		self.data[:] = coords
+		print(self.data)
 
-		self.mouseX = (self.realX[finger] / self.frame.shape[1]) * self.screen_width
-		self.mouseY = (self.realY[finger] / self.frame.shape[0]) * self.screen_height
-		if self.mouseX != 0 and self.mouseY != 0:
-			pyautogui.moveTo(self.mouseX, self.mouseY)
+		# if not self.queue:
+		# 	self.queue.append([self.realX[finger], self.realY[finger]])
+		# 	self.mouseX, self.mouseY = self.realX[finger], self.realY[finger]
+		# 	return
+		# if len(self.queue) == 2:
+		# 	del self.queue[0]
+		# self.queue.append([self.realX[finger], self.realY[finger]])
+		# self.dx = (self.queue[1][0] - self.queue[0][0])**2
+		# self.dy = (self.queue[1][1] - self.queue[0][1])**2
+
+		# self.mouseX = (self.realX[finger] / self.frame.shape[1]) * self.screen_width
+		# self.mouseY = (self.realY[finger] / self.frame.shape[0]) * self.screen_height
+		# if self.mouseX != 0 and self.mouseY != 0:
+		# 	pyautogui.moveTo(self.mouseX, self.mouseY)
 		
 	def __check_pinch(self):
 		fingerDist = math.sqrt((self.realX[self.FINGER1] - self.realX[self.FINGER2])**2 + \
@@ -130,29 +190,33 @@ class Vision():
 		# cv2.drawContours(self.canvas, [self.hullPoints], 0, (255, 0, 0), 2)
 
 	def __frame_outputs(self, finger):
-		if finger == self.FINGER1:
-			cv2.imshow('Canvas: ' + finger, self.canvas[finger])
-		# cv2.imshow('Output ' + finger, self.output[finger])
-		# cv2.imshow('Frame', self.frame)
+		# if finger == self.FINGER1:
+		# 	cv2.imshow('Canvas: ' + finger, self.canvas[finger])
+		cv2.imshow('Output ' + finger, self.output[finger])
+		cv2.imshow('Frame', self.frame)
 
 	def start_process(self):
 		while True:
-			for finger in self.FINGERS:
+			for finger in self.ACTIVE_FINGERS:
 				self.__read_webcam()
-				self.__add_color_threshold(finger)
-				self.__extract_contours(finger)
-				if self.foundContour[finger]:
-					self.__find_center(finger)
+				self.__threshold(finger)
+				# self.__extract_contours(finger)
+				# if self.foundContour[finger]:
+					# self.__find_center(finger)
+				self.__frame_outputs(finger)
 
-			for finger in self.FINGERS:
-				pass
+			# coords = ()
+			# for finger in self.TOTAL_FINGERS:
+				# coords += self.handMoment[finger]
+			
+			# self.__update_cursor(coords, self.TRACKER_FINGER)
+
 
 			if cv2.waitKey(1) & 0xFF is ord('q'):
 				break
 		cv2.destroyAllWindows()
 
-'''******************** METHODS FOR ENTIRE HAND TRACKING ********************'''
-
+	'''**************** METHODS BELOW ARE FOR HAND PALM TRACKING ****************'''	
 	def __get_contour_dimensions(self):
 		self.minX, self.minY, self.handWidth, self.handHeight = \
 			cv2.boundingRect(self.handContour)
