1f95ab81d941d35dbccacea6fe3324678af4f813
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-02-18 09:38:31 -0600

    Update to test changes to sensor API.

--- sensorapi/test.py
+++ sensorapi/test.py
@@ -1,9 +1,13 @@
+#!../venv/bin/python
 import numpy as np
 from glovelet.utility.map import Map
 from glovelet.sensorapi.sensorstream import SensorStream, SensorDataMonitor
 import glovelet.sensorapi.sensorstream as sensorstream
-from glovelet.sensorapi.sensor import FlexSensor, FlexSensorGroup, ImuSensor, Sensor
+from glovelet.sensorapi.sensor import Sensor
+from glovelet.sensorapi.glovelet_sensormonitor import FLEX_SENSORS, GloveletBNO055IMUSensorMonitor
+from multiprocessing import Process
 import argparse
+import time
 from random import randrange
 from serial.serialutil import SerialException
 
@@ -14,6 +18,13 @@ CHANNEL_WIDTH = 6
 
 
 np.set_printoptions(precision=4)
+
+
+def loop(sensor_stream, monitor):
+    while sensor_stream.is_open():
+        sensor_stream.update()
+        # print('acc:' + str(monitor.get_acceleration()) + ' : rot:' + str(monitor.get_rotation().value) )
+
 
 
 def get_u_test(sz=20, mx=50):
@@ -45,9 +56,6 @@ class MapTest(Map):
         print('okay')
 
 
-flex_index = FlexSensor('index', 0)
-
-
 class TestMonitor(SensorDataMonitor):
     def __init__(self):
         pass
@@ -56,31 +64,35 @@ class TestMonitor(SensorDataMonitor):
         print(data)
 
 
-if __name__ == '__main__':
-    # parse args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('port', type=str, help='Path to serial port.')
-    parser.add_argument('baudrate', type=int, help='Baud rate of serial device.')
-    parser.add_argument('channel_width', type=int,
-                        help='Channel width. This is the number of data elements expected to be read on each line of serial \
-                        device output.')
-    args = parser.parse_args()
-    print(args)
-    # set globals
-    PORT = args.port
-    BAUDRATE = args.baudrate
-    CHANNEL_WIDTH = args.channel_width
-    # initialize and open serial data stream
-    stream = SensorStream(PORT, BAUDRATE, CHANNEL_WIDTH)
+def stream_test(port, baud):
+    stream = SensorStream(port, baud)
     stream.open()
-    while stream.is_open():
-        print(stream.get_data())
-else:
-    stream = SensorStream(PORT, BAUDRATE, CHANNEL_WIDTH)
+    monitor = GloveletBNO055IMUSensorMonitor()
+    stream.register_monitor(monitor)
     try:
         stream.open()
     except SerialException as e:
         print(e)
         stream.close()
-    monitor = TestMonitor()
-    stream.register_monitor(monitor, sensorstream.CH_ACCELEROMETER)
+    while(stream.is_open()):
+        stream.update()
+        print('acc:' + str(monitor.get_acceleration()) + ' : rot:' + str(monitor.get_rotation().value) )
+        time.sleep(0.004)
+
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', '-p', type=str, default=PORT, help='Path to serial port. Default path is /dev/ttyACM0.')
+    parser.add_argument('--baudrate', '-b', type=int, default=BAUDRATE, help='Baud rate of serial device. Default is 115200')
+    args = parser.parse_args()
+    print(args)
+    # set globals
+    PORT = args.port
+    BAUDRATE = args.baudrate
+    # Run SesnorStream test
+    stream_test(PORT, BAUDRATE)
+else:
+    stream = SensorStream(PORT, BAUDRATE)
+    monitor = GloveletBNO055IMUSensorMonitor()
+    print()
