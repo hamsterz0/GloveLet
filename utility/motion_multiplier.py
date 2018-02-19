ad8d4a9778a18bfbb819098d5b0c3c951bbf55f0
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-02-18 19:22:36 -0600

    Refactor redundant code.

--- utility/motion_multiplier.py
+++ utility/motion_multiplier.py
@@ -18,14 +18,11 @@ def motion_multiplier(prev_coord, delta_real_coord, k_max=20, k_min=4, n=2, m=1)
     and the initial value of `prev_coord` will be the value of the real frame coordinate.\t
     :param delta_real_coord: this is the change real frame coordinate, r - r_prev\t
     :param k_max: the upper-threshold value for delta_real_coord\t
-    :param k_min: the lower-threshold value for delta_real_coord
+    :param k_min: the lower-threshold value for delta_real_coord\t
+    :param n: `int` the scale factor by which to scale values equal to or greater than `k_max`.
+    Where `1` is `100%` scale factor.
     """
-    res = None
-    if isinstance(prev_coord, Iterable):
-        res = prev_coord + delta_real_coord * [(1 + u(abs(dr), k_max, k_min, n, m)) for dr in delta_real_coord]
-    else:
-        res = prev_coord + delta_real_coord * (1 + u(abs(delta_real_coord), k_max, k_min, n, m))
-    return res
+    return prev_coord + delta_scale(delta_real_coord, k_max, k_min, n, m)
 
 def delta_scale(delta, k_max=20, k_min=4, n=2, m=1):
     res = None
