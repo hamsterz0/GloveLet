98580806d819ecb8ee16d02fe99c63f6f583bc71
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-02-24 11:50:08 -0600

    test.py:  Add additionally tools for testing timeseries module and motion_multiplier.

--- sensorapi/test.py
+++ sensorapi/test.py
@@ -1,12 +1,12 @@
 #!../venv/bin/python
 import numpy as np
 from glovelet.utility.map import Map
-from glovelet.utility.motion_multiplier import motion_multiplier, u
-from glovelet.utility.timeseries import DataTimeSeries
+from glovelet.utility.motion_multiplier import motion_multiplier, u, delta_scale
+from glovelet.utility.timeseries import DataTimeSeries, DataSequence
 from glovelet.sensorapi.sensorstream import SensorStream, SensorDataMonitor
 import glovelet.sensorapi.sensorstream as sensorstream
 from glovelet.sensorapi.sensor import Sensor
-from glovelet.sensorapi.glovelet_sensormonitor import FLEX_SENSORS, GloveletBNO055IMUSensorMonitor
+from glovelet.sensorapi.glovelet_sensormonitor import GloveletBNO055IMUSensorMonitor
 from multiprocessing import Process
 import argparse
 import time
@@ -26,7 +26,6 @@ def loop(sensor_stream, monitor):
     while sensor_stream.is_open():
         sensor_stream.update()
         # print('acc:' + str(monitor.get_acceleration()) + ' : rot:' + str(monitor.get_rotation().value) )
-
 
 
 def get_u_test(sz=20, mx=50):
@@ -78,7 +77,23 @@ def stream_test(port, baud):
     while(stream.is_open()):
         stream.update()
         print('acc:' + str(monitor.get_acceleration()) + ', acc_norm:' + str(monitor.get_acceleration_norm()) + ' : rot:' + str(monitor.get_rotation().value))
-        time.sleep(0.1)
+
+
+def dscale_test(stream, bno055, nsamples=50, args=(), kwargs={}):
+    if not stream.is_open():
+        stream.open()
+        time.sleep(5.0)
+    for i in range(nsamples):
+        stream.update()
+        acc = bno055.get_acceleration()
+        acc_scale = delta_scale(acc, *args, **kwargs)
+        print(str(acc) + " : " + str(acc_scale))
+
+
+def print_velocity(stream, bno055, times=50):
+    for i in range(times):
+        stream.update()
+        print(str(bno055.get_velocity()) + ' :: {: .4f}'.format(monitor.get_time_delta()))
 
 
 if __name__ == '__main__':
@@ -96,5 +111,10 @@ if __name__ == '__main__':
 else:
     stream = SensorStream(PORT, BAUDRATE)
     monitor = GloveletBNO055IMUSensorMonitor()
+    stream.register_monitor(monitor)
+    stream.open()
+    s = stream
+    m = monitor
     v = np.array([0.12, -0.07, 0.21], 'f')
     dr = np.array([0.02, -0.05, 0.10], 'f')
+    dr_scale = (5.0, 0.15, 0.5)
