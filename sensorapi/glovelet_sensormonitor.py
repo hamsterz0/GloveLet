6bfd07e0d24b7df74932215c28b33d5ecab9531d
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-02-24 11:41:06 -0600

    Add GloveletFlexSensorMonitor for monitoring flex sensor data channels using SensorStream.
    Change to `get_velocity` method of GloveletBNO055IMUSensorMonitor class, for now.

--- sensorapi/glovelet_sensormonitor.py
+++ sensorapi/glovelet_sensormonitor.py
@@ -25,21 +25,6 @@ class Mpu6050Sensor(Sensor):
 class FingerFlexGroup(Sensor):
     def __init__(self, name, size):
         super().__init__(name, size)
-        self.__thumb = 0
-        self.__index = 1
-        self.__middle = 2
-
-    @property
-    def thumb(self):
-        return self.__thumb
-
-    @property
-    def index(self):
-        return self.__index
-
-    @property
-    def middle(self):
-        return self.__middle
 
 
 def acc_axis_correction(time_series):
@@ -63,12 +48,12 @@ FLEX_SENSORS = FingerFlexGroup('flexsensors', 3)
 
 
 class GloveletBNO055IMUSensorMonitor(SensorDataMonitor):
-    def __init__(self, series_sz=20):
+    def __init__(self, acc_series_sz=20, rot_series_sz=5):
         super().__init__(BNO055)
-        self.__acc_timeseries = DataTimeSeries(series_sz, 3,
+        self.__acc_timeseries = DataTimeSeries(acc_series_sz, 3,
                                            auto_filter=True,
                                            post_filter=acc_axis_correction)
-        self.__rot_timeseries = DataTimeSeries(series_sz, 4,
+        self.__rot_timeseries = DataTimeSeries(rot_series_sz, 4,
                                            auto_filter=True, filter_alg=None,
                                            post_filter=rot_axis_correction)
         self.__velocity = np.zeros(3, dtype='f')
@@ -82,49 +67,46 @@ class GloveletBNO055IMUSensorMonitor(SensorDataMonitor):
         return acceleration
 
     def get_rotation(self):
-        # self.__data_lock.acquire()
         data = self.__rot_timeseries[0]
         rotation = glm.tquat(*data)
-        # self.__data_lock.release()
         return rotation
 
     def get_tdelta(self):
-        # self.__data_lock.acquire()
         dt = self.__acc_timeseries.tdelta[0]
-        # self.__data_lock.release()
         return dt
 
     def get_timestamp(self):
-        # self.__data_lock.acquire()
         dt = self.__acc_timeseries.timestamp[0]
-        # self.__data_lock.release()
         return dt
 
     def get_velocity(self):
         dt = self.__acc_timeseries.tdelta[0]
-        # self.__data_lock.acquire()
         acc = delta_scale(self.get_acceleration(), 200.0, 0.15, 0)
-        # self.__data_lock.release()
-        bin_acc = np.zeros(3, 'd')
-        for i in range(len(acc)):
-            bin_acc[i] = binary_convert(acc[i])
-        self.__velocity[:] *= bin_acc[:]
+        # bin_acc = np.zeros(3, 'd')
+        # for i in range(len(acc)):
+        #     bin_acc[i] = binary_convert(acc[i])
+        # self.__velocity[:] *= bin_acc[:]
         self.__velocity[:] += (acc * dt)
         return self.__velocity
 
     def get_acceleration_norm(self):
-        # self.__data_lock.acquire()
         acc = self.get_acceleration()
-        # self.__data_lock.release()
         acc_norm = np.linalg.norm(acc)
         return acc_norm
 
     def is_moving(self, norm_threshold=0.19):
-        # self.__data_lock.acquire()
         acc_norm = self.get_acceleration_norm()
-        # self.__data_lock.release()
         is_moving = (acc_norm > norm_threshold)
         return is_moving
+
+
+class GloveletFlexSensorMonitor(SensorDataMonitor):
+    def __init__(self, series_sz=10):
+        super().__init__(FLEX_SENSORS)
+        self.__timeseries = DataTimeSeries(series_sz, FLEX_SENSORS.ndatapoints, auto_filter=True)
+
+    def get_flex_data(self):
+        return self.__timeseries[0]
 
 
 class GloveletMPU6050IMUSensorMonitor(SensorDataMonitor):
