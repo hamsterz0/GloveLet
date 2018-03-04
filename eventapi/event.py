d2d03161ee9d4f17ef3e50e63251a098a672b593
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-03-04 15:10:15 -0600

    event.py: Revise classes to be abstract, inheritable classes and interfaces.
    	- Move test classes to event_example.py
    
    Add event_example.py for example on how to use the classes in event.py
    Add __init__.py for eventapi directory.

--- eventapi/event.py
+++ eventapi/event.py
@@ -1,25 +1,36 @@
 from multiprocessing import Queue, Process
-from random import randrange
-from time import sleep
 
 
-class ExampleEvent:
-    def __init__(self, msg):
-        self.msg = msg
+__all__ = ['Event', 'EventDispatcher', 'EventListener']
 
 
-class Example2Event:
-    def __init__(self, msg):
-        self.msg = msg
+class Event:
+    pass
+
+class EventListener:
+    def __init__(self, callbacks={}):
+        type_err = '`EventListener` constructor requires a dict of `Event` \
+                        type as key and `callable` callback function as value.'
+        if not isinstance(callbacks, dict):
+            raise TypeError(type_err)
+        for event in callbacks:
+            if not callable(callbacks[event]) or not issubclass(event, Event):
+                raise TypeError(type_err)
+        self.event_callbacks = callbacks
 
 
 class EventDispatcher:
-    def __init__(self):
-        self.event_types = (type(ExampleEvent), type(Example2Event))
-        self.event_queues = {self.event_types[0]: Queue(1), self.event_types[1]: Queue(1)}
-        self.listeners = {self.event_types[0]: list(), self.event_types[1]: list()}
-        self.process = Process(target=self.run,\
-            args=(self.event_queues[self.event_types[0]], self.event_queues[self.event_types[1]]))
+    def __init__(self, events, queue_sz=1):
+        self.event_types = tuple(events)
+        self.event_queues = dict()
+        self.listeners = dict()
+        for event in self.event_types:
+            if not issubclass(event, Event):
+                raise TypeError(str(event) + ' is not of type ' + str(Event))
+            self.event_queues[event] = Queue(queue_sz)
+            self.listeners[event] = list()
+        _args = tuple([self.event_queues[x] for x in self.event_types])
+        self.process = Process(target=self.run, args=_args)
 
     def start(self):
         self.process.start()
@@ -30,45 +41,20 @@ class EventDispatcher:
     def register(self, listener):
         for event_type in listener.event_callbacks:
             if event_type in self.listeners:
+                if not issubclass(listener.__class__, EventListener):
+                    raise TypeError(str(listener) + ' is not of type ' + str(EventListener))
                 self.listeners[event_type].append(listener)
 
     def dispatch(self):
         for event_type in self.listeners:
             if not self.event_queues[event_type].empty():
+                print("dispatching: " + str(event_type))
                 for listener in self.listeners[event_type]:
-                    e = None
-                    if event_type == type(ExampleEvent):
-                        e = ExampleEvent('example 1')
-                    elif event_type == type(Example2Event):
-                        e = ExampleEvent('example 2')
+                    e = self.event_queues[event_type].get()
                     listener.event_callbacks[event_type](e)
+        print('dispatched')
 
-    def run(self, exmpl_queue, exmpl2_queue):
-        for i in range(25):
-            sleep(0.5)
-            r = randrange(0,2)
-            if r == 1:
-                exmpl_queue.put(ExampleEvent())
-            else:
-                exmpl2_queue.put(Example2Event)
-
-
-class EventListener:
-    pass
-
-
-class ExampleEventListener(EventListener):
-    def __init__(self, on_example, on_example2):
-        if isinstance(on_example, callable) and isinstance(on_example2, callable):
-            self.event_callbacks = {type(ExampleEvent): on_example, type(Example2Event): on_example2}
-
-
-class TestEventListener(ExampleEventListener):
-    def __init__(self):
-        super().__init__(self.on_example, self.on_example2)
-
-    def on_example(self, event):
-        print(event.msg)
-
-    def on_example2(self, event):
-        print(event.msg)
+    # `run()` should take the same number of queues as its arguments as
+    # the dispatcher has types of events
+    def run(self):
+        raise NotImplementedError('`run()` is abstract method that must be implemented per `EventDispatcher`.')
