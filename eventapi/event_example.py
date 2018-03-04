238814c42813f738e3a39eede74de58d21d44754
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-03-04 16:11:18 -0600

    Revise example EventListener to handle class member value access in a multiprocess safe way.

--- eventapi/event_example.py
+++ eventapi/event_example.py
@@ -1,6 +1,7 @@
 from glovelet.eventapi.event import EventListener, Event, EventDispatcher
 from time import sleep
 from random import randrange
+from multiprocessing import Queue
 
 
 class Example1Event(Event):
@@ -16,20 +17,22 @@ class Example2Event(Event):
 # Example dispatcher
 class ExampleEventDispatcher(EventDispatcher):
     def __init__(self):
-        super().__init__([Example1Event, Example2Event])
+        super().__init__([Example1Event, Example2Event], queue_sz=5)
 
     def run(self, exmpl_queue, exmpl2_queue):
-        for i in range(25):
-            sleep(0.1)
+        count = 1
+        for i in range(26):
+            sleep(0.01)
             r = randrange(0,2)
             if r == 1:
                 if exmpl_queue.full():
                     exmpl_queue.get()
-                exmpl_queue.put(Example1Event('example 1'))
+                exmpl_queue.put(Example1Event('example1 {0: 2d}'.format(count)))
             else:
                 if exmpl2_queue.full():
                     exmpl2_queue.get()
-                exmpl2_queue.put(Example2Event('example 2'))
+                exmpl_queue.put(Example1Event('example2 {0: 2d}'.format(count)))
+            count += 1
             self.dispatch()
 
 
@@ -42,12 +45,29 @@ class ExampleEventListener(EventListener):
 class MyClassThatListensAndStuff(ExampleEventListener):
     def __init__(self):
         super().__init__(self.on_exmpl1, self.on_exmpl2)
+        self.count = 0
+        self.__msg_q = Queue(1)
+        self.__msg = str()
+
+    @property
+    def msg(self):
+        if not self.__msg_q.empty():
+            self.__msg = self.__msg_q.get()
+        return self.__msg
 
     def on_exmpl1(self, event):
-        print(event.msg1)
+        # print(event.msg1 + ': {}'.format(self.count))
+        self.count += 1
+        if self.__msg_q.full():
+            self.__msg_q.get()
+        self.__msg_q.put(event.msg1)
 
     def on_exmpl2(self, event):
-        print(event.msg2)
+        # print(event.msg2 + ': {}'.format(self.count))
+        self.count += 1
+        if self.__msg_q.full():
+            self.__msg_q.get()
+        self.__msg_q.put(event.msg2)
 
 
 if __name__ == '__main__':
@@ -55,5 +75,7 @@ if __name__ == '__main__':
     D = ExampleEventDispatcher()
     D.register(L)
     D.start()
-    sleep(2.75)
+    for i in range(4):
+        sleep(0.08)
+        print(L.msg)
     print('DONE!')
