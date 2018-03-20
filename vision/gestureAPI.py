8cea2ed5787e326ff4b69a384391117e19d628c2
thearnavgarg <thearnavgarg@gmail.com>
2018-03-20 00:08:47 -0500

    Correct changes in the gesture recog code Haven't tested anything yet. Just passes the PEP8 standard and got rid of all the errors shown by the linter.

--- vision/gestureAPI.py
+++ vision/gestureAPI.py
@@ -1,11 +1,12 @@
-from glovelet.vision.storeGesture import StoreGestures
+from glovelet.vision.gesture import Gesture
 import numpy as np
 
 
-class Gesture:
+class PreDefinedGestures:
     def __init__(self):
         self.point_count = 256
         self.predefined_gestures = []
+        self.__circle_gesture() # create the gesture
 
     def __circle_gesture(self):
         radius = 512
@@ -13,6 +14,7 @@ class Gesture:
                      for t in np.linspace(0, 2*math.pi, num=self.point_count)]
         c_points = [(radius*math.cos(t), -radius*math.sin(t))
                     for t in np.linspace(0, 2*math.pi, num=self.point_count)]
-        counter_clockwise = StoreGestures(cc_points, "Counter Clockwise")
-        clockwise = StoreGestures(c_points, "Clockwise")
+        counter_clockwise = StoreGestures(cc_points, "CCW Circle")
+        clockwise = StoreGestures(c_points, "CW Circle")
         self.predefined_gestures += [counter_clockwise, clockwise]
+
