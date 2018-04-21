bd0383a68d3200928c75158fa17c54fd23149749
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-04-21 14:13:56 -0500

    main.py: Add click functionality.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from glovelet.eventapi.event import EventDispatchManager, EventListener
 from glovelet.eventapi.glovelet_hardware_events import GloveletSensorEventDispatcher, GloveletImuEvent, GloveletFlexEvent
-from pyautogui import click
+from pyautogui import mouseDown, mouseUp
 
 
 class SensorListener(EventListener):
@@ -26,13 +26,36 @@ class SensorListener(EventListener):
         self.thumb0 = sum(event.thumb0) / len(event.thumb0)
         self.thumb1 = sum(event.thumb1) / len(event.thumb1)
         if self.left_click_down:
-            if self.index < 0.78:
-                
-        elif self.thumb0 > 0.75:
+            if self.index < 0.65 and self.thumb0 < 0.75:
+                self.left_click_down = False
+                mouseUp(button='left')
+                print('left up')
+        elif self.right_click_down:
+            if self.middle < 0.65 and self.thumb0 < 0.72:
+                self.right_click_down = False
+                mouseUp(button='right')
+                print('right up')
+        elif self.thumb0 > 0.80:
             if not self.left_click_down and not self.right_click_down:
-                if self.index > 0.8 and not self.middle > 0.8:
+                if self.index > 0.70 and not self.middle > 0.70:
                     self.left_click_down = True
-                    click(button='left')
-                elif self.middle > 0.8 and not self.index > 0.8:
+                    mouseDown(button='left')
+                    print('left click')
+                elif self.middle > 0.70 and not self.index > 0.70:
                     self.right_click_down = True
-                    click(button='right')
+                    mouseDown(button='right')
+                    print('right click')
+
+
+def main():
+    sensor_disp = GloveletSensorEventDispatcher('/dev/ttyACM0', 115200)
+    sensor_list = SensorListener()
+    event_mgr = EventDispatchManager(sensor_disp, sensor_list)
+    event_mgr.deploy_dispatchers()
+    while True:
+        event_mgr.invoke_dispatch()
+    event_mgr.end_dispatchers()
+
+
+if __name__ == '__main__':
+    main()
