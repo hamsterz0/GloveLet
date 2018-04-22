07fa3162cb08dc69157201b402e938c9f95bcf4c
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2018-04-22 13:15:37 -0500

    Change left click down to index/no-thumb gesture.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from glovelet.eventapi.event import EventDispatchManager, EventListener
 from glovelet.eventapi.glovelet_hardware_events import GloveletSensorEventDispatcher, GloveletImuEvent, GloveletFlexEvent
 from glovelet.eventapi.glovelet_vision_events import GloveletVisionEventDispatcher, GloveletVisionListener, GloveletVisionEvent
-from pyautogui import mouseDown, mouseUp
+from pyautogui import mouseDown, mouseUp, click
 import pyautogui
 
 
@@ -28,25 +28,32 @@ class GloveletListener(EventListener):
         self.thumb0 = sum(event.thumb0) / len(event.thumb0)
         self.thumb1 = sum(event.thumb1) / len(event.thumb1)
         if self.left_click_down:
-            if self.index < 0.65 and self.thumb0 < 0.75:
+            if self.index < 0.70 and self.thumb0 < 0.75:
                 self.left_click_down = False
                 mouseUp(button='left')
                 print('left up')
         elif self.right_click_down:
-            if self.middle < 0.65 and self.thumb0 < 0.72:
+            if self.middle < 0.70 and self.thumb0 < 0.72:
                 self.right_click_down = False
                 mouseUp(button='right')
                 print('right up')
         elif self.thumb0 > 0.80:
             if not self.left_click_down and not self.right_click_down:
-                if self.index > 0.70 and not self.middle > 0.70:
+                if self.index > 0.80 and not self.middle > 0.65:
                     self.left_click_down = True
-                    mouseDown(button='left')
-                    print('left click')
-                elif self.middle > 0.70 and not self.index > 0.70:
+                    click(clicks=2, button='left')
+                    print('left double')
+                elif self.middle > 0.80 and not self.index > 0.65:
                     self.right_click_down = True
                     mouseDown(button='right')
                     print('right click')
+        # Left button down
+        elif self.index > 0.85 and not (self.left_click_down or self.right_click_down or self.middle > 0.65):
+            self.left_click_down = True
+            print('left click')
+            mouseDown(button='left')
+
+
 
     def on_vision_event(self, event):
         print('{} {}'.format(event.x, event.y))
