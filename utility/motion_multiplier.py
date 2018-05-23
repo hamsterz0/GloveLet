8e2b822e6db99d304909f6236572010b86158543
joseph.tompkins <joseph.tompkins@gmail.com>
2018-05-23 17:38:08 -0500

    motion_multiplier.py: Add script information to top of file.

--- utility/motion_multiplier.py
+++ utility/motion_multiplier.py
@@ -1,6 +1,14 @@
-from collections import Iterable
-
+# Author:       Joseph Tompkins
+# Description:  This was an algorithm developed to adjust CV tracking motion, which
+#               penalizes slow movements so that users have less gitter and finer
+#               control, while rewarding faster movements so that users can move the cursor
+#               across the screen quickly without accidentally exiting the frame and lose
+#               tracking. This method was not used in the end, because it was found that
+#               1-to-1 motion tracking felt more natural, and this algorithm would require
+#               too much calibration to find a happy medium.
 __all__ = ["motion_multiplier", "u", "delta_scale"]
+
+from collections import Iterable
 
 
 def u(dr, k_max=20, k_min=4, n=2, m=1):
@@ -13,14 +21,13 @@ def u(dr, k_max=20, k_min=4, n=2, m=1):
 
 def motion_multiplier(prev_coord, delta_real_coord, k_max=20, k_min=4, n=2, m=1):
     """
-    Multiplier that penalizes slow movements and multiplies faster movements.\n
+    Multiplier that penalizes slow movements and rewards faster movements.\n
     :param prev_coord: The previously calculated coordinate. This will be the result of the last output of `motion_multiplier`
     and the initial value of `prev_coord` will be the value of the real frame coordinate.\t
     :param delta_real_coord: this is the change real frame coordinate, r - r_prev\t
     :param k_max: the upper-threshold value for delta_real_coord\t
     :param k_min: the lower-threshold value for delta_real_coord\t
-    :param n: `int` the scale factor by which to scale values equal to or greater than `k_max`.
-    Where `1` is `100%` scale factor.
+    :param n: `int` the scale factor by which to scale values equal to or greater than `k_max`. Where `1` is `100%` scale factor.
     """
     return prev_coord + delta_scale(delta_real_coord, k_max, k_min, n, m)
 
